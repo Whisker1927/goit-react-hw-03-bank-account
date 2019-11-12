07ea6654c6f6197f1250970d085c697c3c849b00
Vladyslav Bezkrylyi <whisker1927@gmail.com>
2019-11-12 21:31:56 +0200

    Updates

---
+++ precache-manifest.c9fd213017e961781bddedc72400594e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef549f09b70df4e9fa20edfc64c2d0ac",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "60e79f220e6472acd5bc",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.214857a6.chunk.css"
+  },
+  {
+    "revision": "37940c616417790b0265",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.2343c422.chunk.js"
+  },
+  {
+    "revision": "60e79f220e6472acd5bc",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.0babab89.chunk.js"
+  },
+  {
+    "revision": "0c18147979c7c79bba04",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.eaa3779c.js"
+  }
+]);
